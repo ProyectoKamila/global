75aefc1df6fe25ac52213556eb0850c351cfad19
jfigueroapcs <jfigueroaubv@gmail.com>
2015-08-21 20:08:59 +0000

    themplate test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'view/home.php';
 include 'view/play.php'; 
 include 'view/navigation.php'; 
 include 'view/intro.php'; 
-//include 'view/aliados.php'; 
+///include 'view/aliados.php'; 
 include 'view/directivos.php'; 
 include 'view/programacion.php'; 
 
